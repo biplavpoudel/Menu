d489dd1d849677a33c6cf4c20f700eb619a1af74
biplavpoudel <biplavpoudel764@gmail.com>
2024-09-07 18:45:58 +0545

    Added Details Page

--- Menu/Controllers/Menu.cs
+++ Menu/Controllers/Menu.cs
@@ -17,4 +17,17 @@ public class Menu : Controller
     {
         return View(await _context.Dishes.ToListAsync());
     }
+    public async Task<IActionResult> Details(int? id)
+    {
+        var dish = await _context.Dishes
+            .Include(di => di.DishIngredients)
+            .ThenInclude(i => i.Ingredient)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (dish == null)
+        {
+            return NotFound();
+        }
+        return View(dish);
+    }
 }
